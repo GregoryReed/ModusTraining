505d90709bda0851641bdfb28ca5caeeafb6e1df
markdavidmc0 <mark@dotmodus.com>
2018-04-16 08:46:15 +0200

    Updated regex and return statements.

--- decompression/comp_decomp.py
+++ decompression/comp_decomp.py
@@ -12,14 +12,17 @@ def decompress(tight_object):
     return: tight_object string
     """
     # Find num / value pairs of inner-most compressions
-    inners = re.findall(r"\d+\[+\w+]", tight_object)
+    inners = re.findall(r"\d+\[+\w+]|\d+\[+]", tight_object)
 
     # Replace num / value pairs with expanded pairs
     for num_value in inners:
         tight_object = tight_object.replace(num_value, expansion(num_value))
 
     # Expand inner-most compressions
-    return tight_object if not inners else decompress(tight_object)
+    if not inners:
+        return tight_object
+    else:
+        return decompress(tight_object)
 
 def expansion(pair):
     """ Extract the number in front of a bracketed string and multiply the
@@ -41,34 +44,34 @@ def test():
     answer = decompress(test0)
     assert answer == 'aaaaaaaaaa', \
         "answer was {}, not aaaaaaaaaa: ".format(answer)
-    print(answer)
 
     # nested
     test1 = '2[3[a]b]'
     answer = decompress(test1)
     assert answer == 'aaabaaab', \
         "answer was {}, not aaabaaab: ".format(answer)
-    print(answer)
 
     # recursive
     test2 = 'long2[chain]withlots3[3[of]chain]1[and2[some2[recursion]]]'
     answer = decompress(test2)
     assert answer == 'longchainchainwithlotsofofofchainofofofchainofofofchainandsomerecursionrecursionsomerecursionrecursion', \
         "answer was {}, not longchainchainwithlotsofofofchainofofofchainofofofchainandsomerecursionrecursionsomerecursionrecursion: ".format(answer)
-    print(answer)
 
     # banger
     test3 = '2[2[2[2[2[2[a]b]c]d]e]f]g'
     answer = decompress(test3)
     assert answer == 'aabaabcaabaabcdaabaabcaabaabcdeaabaabcaabaabcdaabaabcaabaabcdefaabaabcaabaabcdaabaabcaabaabcdeaabaabcaabaabcdaabaabcaabaabcdefg', \
         "answer was {}, not aabaabcaabaabcdaabaabcaabaabcdeaabaabcaabaabcdaabaabcaabaabcdefaabaabcaabaabcdaabaabcaabaabcdeaabaabcaabaabcdaabaabcaabaabcdefg: ".format(answer)
-    print(answer)
 
     # by zero
     test4 = '0[abc]'
     answer = decompress(test4)
     assert answer == '', "answer was {}, not".format(answer)
-    print(answer)
+
+    # inner zero
+    test5 = '5[0[abc]]xyz'
+    answer = decompress(test5)
+    assert answer == 'xyz', "answer was {}, not".format(answer)
 
     print("All tests Passed!")
 
@@ -76,7 +79,7 @@ def test():
 def main():
     test()
     t = timeit.Timer(test)
-    print(t.timeit(1))
+    print(t.timeit(200)/100)
 
 
 if __name__ == '__main__':
